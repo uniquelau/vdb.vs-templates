6d5e9bf6d8c0f53ab33c8f0e8d7b215648e21b0b
Laurie <lau@voodoobytes.co.uk>
2014-05-18 12:25:16 +0100

    Renamed from Site => Web

--- 2010/Umbraco/VS.NET/_company_._project_.Web/Properties/AssemblyInfo.cs
+++ 2010/Umbraco/VS.NET/_company_._project_.Web/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("_company_._project_.Site")]
+[assembly: AssemblyTitle("_company_._project_.Web")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("_companyfullname_")]
