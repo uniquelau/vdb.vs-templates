4287967ad3b8432c32266eadb0e9d444a7ebed49
Kristian Schneider <khs@thamesconsulting.com>
2014-12-03 13:39:54 +0000

    Replaced guids

--- 2012/Src/_company_._project_.Web/Properties/AssemblyInfo.cs
+++ 2012/Src/_company_._project_.Web/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("_company_._project_.Web")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("_company_._project_.Web")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("_companyfullname_")]
+[assembly: AssemblyProduct("_company_._project_.Site")]
+[assembly: AssemblyCopyright("Copyright © _companyfullname_ 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4ee1e274-33d1-47e3-987d-cdf3538ebb5b")]
+[assembly: Guid("_guid1_")]
 
 // Version information for an assembly consists of the following four values:
 //
