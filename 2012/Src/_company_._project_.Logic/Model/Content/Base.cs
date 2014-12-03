4cf300256bdfa22d80357788cbe37762cab86f93
Kristian Schneider <khs@thamesconsulting.com>
2014-12-03 12:49:58 +0000

    Basic project setup

---
+++ 2012/Src/_company_._project_.Logic/Model/Content/Base.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Core.Models;
+using Umbraco.Web;
+
+namespace _company_._project_.Logic.Model.Content
+{
+    public class Base : BaseContentType
+    {
+        public Base(IPublishedContent content) : base(content)
+        {
+        }
+
+        #region Meta
+
+        public string ShortNavTitle
+        {
+            get
+            {
+                var navTitle = this.GetPropertyValue<string>("navTitle");
+                return !String.IsNullOrWhiteSpace(navTitle) ? navTitle : Name;
+            }
+        }
+
+        public string MetaTitle
+        {
+            get
+            {
+                var metaTitle = this.GetPropertyValue<string>("metaTitle");
+                return !String.IsNullOrWhiteSpace(metaTitle) ? metaTitle : Name;
+            }
+        }
+
+        public string MetaDescription
+        {
+            get
+            {
+                return this.GetPropertyValue<string>("metaDescription");
+            }
+        }
+
+        public string MetaKeywords
+        {
+            get
+            {
+                return this.GetPropertyValue<string>("metaKeywords");
+            }
+        }
+
+        public bool SiteMapExclude
+        {
+            get
+            {
+                return this.GetPropertyValue<bool>("metaSiteMapExclude");
+            }
+        }
+
+
+        #endregion
+    }
+}
---
+++ 2012/Src/_company_._project_.Logic/Model/Content/BaseContentType.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _company_._project_.Logic.Model.Content
+{
+    using Umbraco.Core.Models;
+    using Umbraco.Core.Models.PublishedContent;
+    using Umbraco.Web;
+    using Umbraco.Web.Security;
+
+    public abstract class BaseContentType : PublishedContentModel
+    {
+        private MembershipHelper membershipHelper;
+
+        protected BaseContentType(IPublishedContent content)
+            : base(content)
+        {
+        }
+
+        /// <summary>
+        /// to match the .Members property found in the SurfaceControllers and the Views
+        /// </summary>
+        protected MembershipHelper Members
+        {
+            get
+            {
+                if (this.membershipHelper == null)
+                {
+                    this.membershipHelper = new MembershipHelper(UmbracoContext.Current);
+                }
+
+                return this.membershipHelper;
+            }
+        }
+    }
+}
