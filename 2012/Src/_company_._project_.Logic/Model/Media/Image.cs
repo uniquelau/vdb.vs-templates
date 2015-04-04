b7f3a75270fcc4a78da29e7e9c97f52b5ac08dd1
Laurie <lau@voodoobytes.co.uk>
2015-04-04 15:19:43 +0100

    Image.cs => Basic Image OM for Umbraco.

---
+++ 2012/Src/_company_._project_.Logic/Model/Media/Image.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Umbraco.Core.Models;
+using Umbraco.Web;
+using _company_._project_.Logic.Model.Content;
+using Umbraco.Web.Models;
+
+namespace _company_._project_.Logic.Model.Media
+{
+		public class Image : BaseContentType
+		{
+
+				public Image(IPublishedContent content)
+						: base(content)
+				{
+				}
+
+				#region Image
+
+				public string AltText
+				{
+						get
+						{
+								return this.GetPropertyValue<string>("altText");
+						}
+				}
+
+				public string UmbracoFile
+				{
+						get
+						{
+								return this.GetPropertyValue<string>("umbracoFile");
+						}
+				}
+
+				public string UmbracoExtension
+				{
+						get
+						{
+								return this.GetPropertyValue<string>("umbracoExtension");
+						}
+				}
+
+				public string UmbracoBytes
+				{
+						get
+						{
+								return this.GetPropertyValue<string>("umbracoBytes");
+						}
+				}
+
+				public string UmbracoWidth
+				{
+						get
+						{
+								return this.GetPropertyValue<string>("umbracoWidth");
+						}
+				}
+
+				public string UmbracoHeight
+				{
+						get
+						{
+								return this.GetPropertyValue<string>("umbracoHeight");
+						}
+				}
+
+				#endregion Image
+
+				
+
+				public string Url
+				{
+						get
+						{
+								return this.GetCropUrl(imageCropMode: ImageCropMode.Max , width: 1200);
+						}
+				}
+
+			
+		}
+}
