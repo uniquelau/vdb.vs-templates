2ecf49bc450f5ff3d05b0cdac8fdd31b001469d2
Laurie <lau@voodoobytes.co.uk>
2015-04-04 15:19:29 +0100

    Implemented Meta Interface.

---
+++ 2012/Src/_company_._project_.Logic/Model/Interfaces/IMeta.cs
@@ -0,0 +1,17 @@
+﻿namespace _company_._project_.Logic.Model.Interfaces
+{
+		using Media;
+
+		public interface IMeta
+		{
+				string ShortNavTitle { get; }
+				string MetaTitle { get; }
+				string MetaDescription { get; }
+				bool SiteMapExclude { get; }
+				string SocialTitle { get; }
+				string SocialDescription { get; }
+				Image SocialImage { get; }
+
+				string OgType { get; }
+		}
+}
