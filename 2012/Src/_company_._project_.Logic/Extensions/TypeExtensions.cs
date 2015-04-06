c4d3db00b92b81841604e98d292f8a9db4bf3747
Laurie <lau@voodoobytes.co.uk>
2015-04-06 12:22:47 +0100

    Logic - This is the magic that makes Umbraco Strongly Typed.

---
+++ 2012/Src/_company_._project_.Logic/Extensions/TypeExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+
+namespace _company_._project_.Logic.Extensions
+{
+		public static class TypeExtensions
+		{
+				/// <summary>
+				/// Uses reflection to get the value of a public constant 'TypeAlias' of the type supplied
+				/// </summary>
+				/// <param name="type">type to identify a specific Model</param>
+				/// <returns>the string value of the Type.Name unless a TypeAlias constant is avaiable</returns>
+				public static string GetTypeAlias(this Type type)
+				{
+						string typeAlias = type.Name; // default return value
+
+						FieldInfo typeAliasFieldInfo = type.GetField("TypeAlias");
+
+						if (typeAliasFieldInfo != null)
+						{
+								// constant found, so get it's value
+								typeAlias = typeAliasFieldInfo.GetValue(null).ToString();
+						}
+
+						return typeAlias;
+				}
+		}
+}
---
+++ 2012/Src/_company_._project_.Logic/Handlers/SetPublishedContentModelFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Umbraco.Core;
+using Umbraco.Core.Models.PublishedContent;
+
+namespace _company_._project_.Logic.Handlers
+{
+		public class SetPublishedContentModelFactory : ApplicationEventHandler
+		{
+				protected override void ApplicationStarting(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+				{
+						IEnumerable<Type> types = PluginManager.Current.ResolveTypes<PublishedContentModel>();
+						var factory = new PublishedContentModelFactory(types);
+						PublishedContentModelFactoryResolver.Current.SetFactory(factory);
+				}
+		}
+}
