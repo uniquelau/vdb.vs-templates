757a6872492031b281f3a211e5ea35aa77fb9ed7
Laurie <lau@voodoobytes.co.uk>
2015-12-15 16:11:27 +0000

    Image.cs - Improved handling of Image Cropper JSON

--- 2012/Src/_company_._project_.Logic/Model/Media/Image.cs
+++ 2012/Src/_company_._project_.Logic/Model/Media/Image.cs
@@ -1,81 +1,93 @@
-﻿using System;
+﻿using Newtonsoft.Json;
 using Umbraco.Core.Models;
 using Umbraco.Web;
-using _company_._project_.Logic.Model.Content;
 using Umbraco.Web.Models;
 
 namespace _company_._project_.Logic.Model.Media
 {
-		public class Image : BaseContentType
-		{
+    using Model.Content;
 
-				public Image(IPublishedContent content)
-						: base(content)
-				{
-				}
+    public class Image : BaseContentType
+    {
 
-				#region Image
+        public Image(IPublishedContent content)
+            : base(content)
+        {
+        }
 
-				public string AltText
-				{
-						get
-						{
-								return this.GetPropertyValue<string>("altText");
-						}
-				}
+        internal static bool IsJson(string input)
+        {
+            input = input.Trim();
+            return (input.StartsWith("{") && input.EndsWith("}"))
+                || (input.StartsWith("[") && input.EndsWith("]"));
+        }
 
-				public string UmbracoFile
-				{
-						get
-						{
-								return this.GetPropertyValue<string>("umbracoFile");
-						}
-				}
+        #region Image
 
-				public string UmbracoExtension
-				{
-						get
-						{
-								return this.GetPropertyValue<string>("umbracoExtension");
-						}
-				}
+        public string AltText
+        {
+            get
+            {
+                return this.GetPropertyValue<string>("altText");
+            }
+        }
 
-				public string UmbracoBytes
-				{
-						get
-						{
-								return this.GetPropertyValue<string>("umbracoBytes");
-						}
-				}
+        public string UmbracoFile
+        {
+            get
+            {
+                var prop = this.GetPropertyValue<string>("umbracoFile");
+                if (IsJson(prop))
+                {
+                    dynamic d = JsonConvert.DeserializeObject(prop);
+                    return d.src;
+                }
+                return prop;
+            }
+        }
 
-				public string UmbracoWidth
-				{
-						get
-						{
-								return this.GetPropertyValue<string>("umbracoWidth");
-						}
-				}
+        public string UmbracoExtension
+        {
+            get
+            {
+                return this.GetPropertyValue<string>("umbracoExtension");
+            }
+        }
 
-				public string UmbracoHeight
-				{
-						get
-						{
-								return this.GetPropertyValue<string>("umbracoHeight");
-						}
-				}
+        public string UmbracoBytes
+        {
+            get
+            {
+                return this.GetPropertyValue<string>("umbracoBytes");
+            }
+        }
 
-				#endregion Image
+        public string UmbracoWidth
+        {
+            get
+            {
+                return this.GetPropertyValue<string>("umbracoWidth");
+            }
+        }
 
-				
+        public string UmbracoHeight
+        {
+            get
+            {
+                return this.GetPropertyValue<string>("umbracoHeight");
+            }
+        }
 
-				public string Url
-				{
-						get
-						{
-								return this.GetCropUrl(imageCropMode: ImageCropMode.Max , width: 1200);
-						}
-				}
+        #endregion Image
 
-			
-		}
+        public string Url
+        {
+            get
+            {
+                return this.GetCropUrl(imageCropMode: ImageCropMode.Max, width: 1200);
+            }
+        }
+
+
+    }
 }
