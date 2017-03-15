3d677ef73eae70841a6ae1833559622e7b29aba9
Laurie <lau@voodoobytes.co.uk>
2017-03-15 10:50:28 +0000

    Removed MSBuild, handle minification using Gulp.

--- 2012/Src/_company_._project_.Web/gulpfile.js
+++ 2012/Src/_company_._project_.Web/gulpfile.js
@@ -10,37 +10,54 @@ var plugins = loadPlugins();
  */
 gulp.task('watch', function () {
 
-	plugins.livereload.listen();
-	gulp.watch('Css/sass/**/*.scss', ['styles']);
-	gulp.watch([
+    plugins.livereload.listen();
+    gulp.watch('Css/sass/**/*.scss', ['styles']);
+    gulp.watch([
 		'Css/*.css',
 		'Scripts/**/*.js',
 		'Views/**/*.cshtml'
-	]).on('change', plugins.livereload.changed);
+    ]).on('change', plugins.livereload.changed);
 });
+
 
 /**
  * Compile SASS to CSS
  */
 gulp.task('styles', function () {
-		return gulp.src('Css/sass/styles.scss')
-			.pipe(plugins.plumber())
-			.pipe(plugins.sourcemaps.init())
-			.pipe(plugins.sass({
-					errLogToConsole: true
-			}))
-			.pipe(plugins.autoprefixer())
-			.pipe(plugins.sourcemaps.write())
-			.pipe(gulp.dest('Css'));
+    return gulp.src('Css/sass/screen.scss')
+        .pipe(plugins.plumber())
+        .pipe(plugins.sourcemaps.init())
+        .pipe(plugins.sass({
+            errLogToConsole: true
+        }))
+        .pipe(plugins.autoprefixer())
+        .pipe(plugins.sourcemaps.write())
+        .pipe(gulp.dest('Css'));
 });
+
+/**
+ * Compile SASS to CSS for use in Umbraco Backoffice
+ */
+gulp.task('styles-umbraco', function () {
+    return gulp.src('Css/sass/umbraco.scss')
+        .pipe(plugins.plumber())
+        .pipe(plugins.sourcemaps.init())
+        .pipe(plugins.sass({
+            errLogToConsole: true
+        }))
+        .pipe(plugins.autoprefixer())
+        .pipe(plugins.sourcemaps.write())
+        .pipe(gulp.dest('Css'));
+});
+
 
 
 /**
  * Load scripts from build:js block in template and minify
  */
 gulp.task('jsmin', function () {
-	return gulp.src('Views/_Layout.cshtml')
-		.pipe(plugins.readmin({type: 'js'}))
+    return gulp.src('Views/_Layout.cshtml')
+        .pipe(plugins.readmin({ type: 'js' }))
 		.pipe(plugins.concat('minified.js'))
 		.pipe(plugins.uglify())
 		.pipe(gulp.dest('scripts'));
@@ -50,13 +67,12 @@ gulp.task('jsmin', function () {
  * Load styles from build:css block in template and minify
  */
 gulp.task('cssmin', function () {
-	return gulp.src('Views/_Layout.cshtml')
-		.pipe(plugins.readmin({type: 'css'}))
+    return gulp.src('Views/_Layout.cshtml')
+		.pipe(plugins.readmin({ type: 'css' }))
 		.pipe(plugins.concat('minified.css'))
-		.pipe(plugins.minifyCss())
+		.pipe(plugins.cssnano())
 		.pipe(gulp.dest('css'));
 });
-
 
 /**
  * Default task
@@ -70,18 +86,23 @@ gulp.task('default', ['styles', 'cssmin', 'jsmin', 'watch']);
 
 gulp.task('build-Debug', ['styles', 'cssmin', 'jsmin']);
 
-gulp.task('build-Release', ['clean' , 'styles' , 'cssmin', 'jsmin']);
+gulp.task('build-Release', ['clean', 'styles', 'cssmin', 'jsmin']);
+
+/**
+* Umbraco CSS work
+*/
+gulp.task('umbraco', ['styles-umbraco', 'watch']);
 
 
 /**
- * Delete generated files
+ * Delete generated/linked files
  */
 gulp.task('clean', function (cb) {
-	try {
-		fs.unlinkSync('css/minified.css');
-		fs.unlinkSync('scripts/minified.js');
-	} catch (err) {
-		//do nothing
-	}
-	cb();
+    try {
+        fs.unlinkSync('css/minified.css');
+        fs.unlinkSync('scripts/minified.js');
+    } catch (err) {
+        //do nothing ;-)
+    }
+    cb();
 });
